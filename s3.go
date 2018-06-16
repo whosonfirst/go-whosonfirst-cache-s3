a419c3502972d4f93f80c63403b2d3a4fbaa400b
thisisaaronland <straup_github@gmail.com>
2018-06-15 17:14:46 -0700

    start implementing more things

--- s3.go
+++ s3.go
@@ -1,28 +1,34 @@
 package s3
 
 import (
-       "github.com/whosonfirst/go-whosonfirst-cache"
-       wof_s3 "github.com/whosonfirst/go-whosonfirst-aws/s3"
+	wof_s3 "github.com/whosonfirst/go-whosonfirst-aws/s3"
+	"github.com/whosonfirst/go-whosonfirst-cache"
 )
 
 type S3Cache struct {
-     cache.Cache
-     conn *wof_s3.S3Connection
+	cache.Cache
+	conn *wof_s3.S3Connection
 }
 
-func NewS3Cache() (cache.Cache, error){
+func NewS3Cache(dsn string) (cache.Cache, error) {
 
-     conn, err := wof_s3.NewS3Connection(&config)
+	cfg, err := wof_s3.NewS3ConfigFromString(dsn)
 
-     if err != nil {
-     	return nil, err
-     }
+	if err != nil {
+		return nil, err
+	}
 
-     c := S3Cache{
-       conn: conn,
-     }
+	conn, err := wof_s3.NewS3Connection(&config)
 
-     return &c, nil
+	if err != nil {
+		return nil, err
+	}
+
+	c := S3Cache{
+		conn: conn,
+	}
+
+	return &c, nil
 }
 
 func (c *S3Cache) Get(key string) (io.ReadCloser, error) {
@@ -32,12 +38,19 @@ func (c *S3Cache) Get(key string) (io.ReadCloser, error) {
 
 func (c *S3Cache) Set(key string, fh io.ReadCloser) (io.ReadCloser, error) {
 
-     	// FIX ME TO RETURN AN io.ReadCloser
+	// PLEASE MAKE ME BETTER - presumably with some
+	// confusing io.WahWah stuff (20180615/thisisaaronland)
 
-	return c.conn.Put(key, fh)
+	err := c.conn.Put(key, fh)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return c.Get(key)
 }
 
 func (c *S3Cache) Unset(key string) error {
 
-     return c.conn.Delete(key)
+	return c.conn.Delete(key)
 }
